0b9a80be76c47c1c63d63e1fc24525d466eb3417
Kathryn-r <Kathryn_R@outlook.com>
2018-07-24 02:14:39 +0800

    准备机试

---
+++ For_Ctest/cmake-build-debug/CMakeFiles/For_Ctest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/For_Ctest.dir/main.cpp.o"
+  "For_Ctest.pdb"
+  "For_Ctest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/For_Ctest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ For_Ctest/cmake-build-debug/CMakeFiles/For_Ctest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for For_Ctest.
+# This may be replaced when dependencies are built.
