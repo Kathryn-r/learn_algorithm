59379e4ded74647933c58fb2da90255cd1546267
Kathryn-r <Kathryn_R@outlook.com>
2018-07-23 23:47:59 +0800

    learning and practicing

---
+++ July_AOP/cmake-build-debug/CMakeFiles/July_AOP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/July_AOP.dir/main.cpp.o"
+  "July_AOP.pdb"
+  "July_AOP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/July_AOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ July_AOP/cmake-build-debug/CMakeFiles/July_AOP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for July_AOP.
+# This may be replaced when dependencies are built.
